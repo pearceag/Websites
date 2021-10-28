8b8a1ab8969e6f44c2a3942eb79975d3879484d9
Lexie Pearce <pearceag@dukes.jmu.edu>
2021-10-28 10:22:14 -0400

    added enpoints to report.html and added cross-orgi

--- project2/index.js
+++ project2/index.js
@@ -8,11 +8,28 @@ const connection = mysql.createConnection(table);
 app = express();
 
 app.use(express.json());
-connection.connect();
+connection.connect(error => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
 
 const port = 5001;
 app.listen(port, () => {
   console.log("We're live on port 5001");
+});
+
+// Cross-orgin Resource Sharing
+app.use((request, response, next) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.options('*', (request, response) => {
+  response.set('Access-Control-Allow-Headers', 'Content-Type');
+  response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  response.sendStatus(200);
 });
 
 function rowToPlaylist(row) {
@@ -26,10 +43,14 @@ function rowToPlaylist(row) {
   };
 }
 
-//Get all info in playlist
+// Takes you to the report page
+app.get('/report.html', (request, response) => {
+  response.sendFile('/home/pearceag/cs347/project2/report.html');
+});
+
+// Selects all items in playlist
 app.get('/playlist', (request, response) => {
   const query = 'SELECT * FROM playlist WHERE is_deleted = 0';
-
   connection.query(query, (error, rows) => {
     if (error) {
       response.status(500);
@@ -47,8 +68,8 @@ app.get('/playlist', (request, response) => {
   });
 });
 
-// Gets the songs by specified artists
-app.get('/playlist/:artist', (request, response) => {
+// Selects all the playlists items with the same artist
+app.get('/playlist/artist/:artist', (request, response) => {
   const parameters = [
     request.params.artist,
   ];
@@ -71,7 +92,80 @@ app.get('/playlist/:artist', (request, response) => {
   });
 });
 
-// DOES NOT WORK RIGHT NOW
+// Selects all the playlist items with the same genre
+app.get('/playlist/genre/:genre', (request, response) => {
+  const parameters = [
+    request.params.genre,
+  ];
+
+  const query = 'SELECT * FROM playlist WHERE genre = ? AND is_deleted = 0';
+  connection.query(query, parameters, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      const songs = rows.map(rowToPlaylist);
+      response.json({
+        ok: true,
+        results: rows.map(rowToPlaylist),
+      });
+    }
+  });
+});
+
+// Selects all the playlist items with the same album
+app.get('/playlist/album/:album', (request, response) => {
+  const parameters = [
+    request.params.album,
+  ];
+
+  const query = 'SELECT * FROM playlist WHERE album = ? AND is_deleted = 0';
+  connection.query(query, parameters, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      const songs = rows.map(rowToPlaylist);
+      response.json({
+        ok: true,
+        results: rows.map(rowToPlaylist),
+      });
+    }
+  });
+});
+
+// Select all the playlist items with the same song name
+app.get('/playlist/songName/:songName', (request, response) => {
+  const parameters = [
+    request.params.songName,
+  ];
+
+  const query = 'SELECT * FROM playlist WHERE songName = ? AND is_deleted = 0';
+  connection.query(query, parameters, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      const songs = rows.map(rowToPlaylist);
+      response.json({
+        ok: true,
+        results: rows.map(rowToPlaylist),
+      });
+    }
+  });
+});
+
+
+// Updates the playlist item indicated by the id param
 app.patch('/playlist/:id', (request, response) => {
   const parameters = [
     request.body.songName,
@@ -97,7 +191,7 @@ app.patch('/playlist/:id', (request, response) => {
   });
 });
 
-// Deletes data at id
+// Deletes item in playlist at id
 app.delete('/playlist/:id', (request, response) => {
   const parameters = [parseInt(request.params.id)];
 
@@ -117,7 +211,7 @@ app.delete('/playlist/:id', (request, response) => {
   });
 });
 
-//Add a new song to playlist with the parameters found in the body of the request.
+//Inserts new item to playlist
 app.post('/playlist/', (request, response) => {
   if (request.body.hasOwnProperty('songName') &&
     request.body.hasOwnProperty('artist') &&
