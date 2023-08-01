65b9fcc4edbcb0bc81b7e31b5f834ab511b5f09f
Lexie Pearce <pearceag@dukes.jmu.edu>
2023-08-01 14:52:04 -0400

    removed unneeded files

--- project2/project2.sql
+++
@@ -1,8 +0,0 @@
--- Remove any existing database and user.
-DROP DATABASE IF EXISTS project2;
-DROP USER IF EXISTS project2_user@localhost;
-
--- Create Unforget database and user. Ensure Unicode is fully supported.
-CREATE DATABASE project2 CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER project2_user@localhost IDENTIFIED WITH mysql_native_password BY 'Patches3221!';
-GRANT ALL PRIVILEGES ON project2.* TO project2_user@localhost;
